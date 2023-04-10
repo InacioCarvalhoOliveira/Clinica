0bb34046f7023e49fd9ebd3037bc137c3abd9286
InacioCarvalhoOliveira <inacio.cavalho.oliveira02@gmail.com>
2023-04-09 23:11:58 -0300

    conteúdo do front

---
+++ src/app/page/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CLIENT_RENEG_WINDOW } from 'tls';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  servicePage(){
+      
+
+  }
+
+  eventPage(){
+   
+  }
+
+  aboutRedirect(){
+   
+  }
+
+  appointmentsPage(){
+   
+  }
+
+
+}
